e0ee2a33b4d047528348019f5cd8264548017cee
veronique-large <100087668+veronique-large@users.noreply.github.com>
2022-05-19 22:08:47 +0200

    Add files via upload

--- front/JS/product.js
+++ front/JS/product.js
@@ -9,7 +9,7 @@ fetch("http://localhost:3000/api/products")
 .then(function(products){
     products.forEach(product => {
 
-var str = "http://localhost:3000/api/products/+'id";
+var str = "http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926"
 var url = new URL(str);
 var id = url.searchParams.get("product");
 console.log(product);
