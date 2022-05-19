5ac30403fee6205cc12e005dd98f8d4ef8b57467
veronique-large <100087668+veronique-large@users.noreply.github.com>
2022-05-19 21:59:07 +0200

    Add files via upload

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,23 +1,20 @@
-fetch("http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926")
-    .then(reponse => reponse.json()) 
-    .then(article => console.table(article))
-    /**creation d'un boucle pour obtenir les informations */
-    /*.then(function (products) {
-            products.forEach(product => {*/
 
-                const item__img = document.getElementsByClassName('item__img');
-                const firstElements = item__img[0];
-                console.log(item__img);
-                let img = document.createElement('img');
-                 img.src = product.imageUrl;
-                 img.alt = product.altTxt;
-                 items.appendChild(img);
-            
-                 let title = document.createElement('title');
-                 title.classList.add("productsName");
-                 title.innerHTML = products.name;
-                 classItem.appendChild(title);
+fetch("http://localhost:3000/api/products")
+.then(function(res) {
+    if (res.ok) {
+        return res.json();
+ }
+})
+/**creation de la boucle pour obtenir les informations de chaque article */
+.then(function(products){
+    products.forEach(product => {
 
-                 
-      /*     })
-        })*/
+var str = "http://localhost:3000/api/products/+'id";
+var url = new URL(str);
+var id = url.searchParams.get("product");
+console.log(product);
+
+
+
+       })
+    })
