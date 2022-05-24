ae88d852613dbf041b39bd2b025272df8edceaeb
veronique-large <100087668+veronique-large@users.noreply.github.com>
2022-05-24 15:41:03 +0200

    Add files via upload
    Ajout du titre, modification début de JS

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,39 +1,41 @@
-let url = new URLSearchParams('http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926');
-const id = url.get('107fb5b75607497b96722bda5b504926');
-const str = 'http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926';
+let url = new URLSearchParams(window.location.search);
+const id = url.get('id');
+const str = 'http://localhost:3000/api/products/'+id;
 
-fetch('http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926')
-    .then(reponse => reponse.json()) 
+fetch(str)
+    /*.then(reponse => reponse.json())*/ 
+    .then(function(reponse) {
+        if (reponse.ok) {
+            return reponse.json()
     .then(reponse2 => console.log(reponse2))
     
-fetch("http://localhost:3000/api/products")
+/*fetch("http://localhost:3000/api/products")
 .then(res => {
     if(res.ok){
         res.json().then(data => {
 })
+    }
+})*/
+
    
-
-/*.then(function(product){
-    product.for(product => {*/
-   
+.then(function(products){ 
+    this.article = products;
+    this.products = id+products;
     
-    /*let .item = document.getElementsByClassName('.item');*/
+            /*let item_img = document.getElementsByClassName('item_img');*/
 
-    let img = document.createElement('productImage');
-    img.src = 'http://localhost:3000/images/kanap01.jpeg';
-    /*item.appendchild(img);*/
+            let img = document.createElement('img');
+            img.classList.add("productImage");
+            img.src = 'http://localhost:3000/images/kanap01.jpeg';
+            /*item_img.appendChild(img);*/
 
-    let h1 = document.createElement('h1');title.innerHTML;
-    h1.innerHTML = ('title');
-    title = ('productName');
-    
-    var p = document.getElementById('price');
-    var Text = document.createTextNode("1849");
-    p.appendChild(Text);
-    
+            let title = document.getElementById('title'); 
+            title.innerHTML = ('kanap Sinopé');
+
+            var p = document.getElementById('price');
+            var Text = document.createTextNode("1849");
+            p.appendChild(Text);
+
+        })
     }
 })
-
-    
-    
-
