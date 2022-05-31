a21f932da218023465f4cad6411eb38cbe0b739e
veronique-large <100087668+veronique-large@users.noreply.github.com>
2022-05-31 17:26:29 +0200

    Add files via upload

--- front/JS/product.js
+++ front/JS/product.js
@@ -29,42 +29,41 @@ fetch(str)
 
             let colors = document.querySelector('#colors');/* je créé une variable pour lui donner comme référence la balise liée à l’ID Colors*/
 
-            product.colors.forEach(color => {/* je créé une boucle pour appeler les données de toutes les couleurs, product correspond au produit, colors à leur valeur,product.colors(identifiant) identifie les couleurs du produit, color(argument) fait reference à la couleur ce qui permet de l'appeler*/
+            product.colors.forEach(color => {/* je cree une boucle pour appeler les donnees de toutes les couleurs, product correspond au produit, colors à leur valeur,product.colors(identifiant) identifie les couleurs du produit, color(argument) fait reference à la couleur ce qui permet de l'appeler*/
                 let optionColor = document.createElement('option');/* je créé une variable que j'appelle optionColor  pour créer l'element option*/
                 colors.appendChild(optionColor);/*je definie la variable optionColor comme étant l'enfant de colors*/
                 optionColor.innerHTML = color;/*J'applique la variable optionColors à l'interieur de HTML, la valeur du mot clé color*/
-                optionColor.value = color; /*J’applique à la variable optionColor, la valeur du mot clé color (paramètre dans la boucle ligne 31) */     
+                optionColor.value = color; /*J’applique à la variable optionColor, la valeur du mot cle color (paramètre dans la boucle ligne 31) */     
             });/*la fermeture du bloc*/
 
-            let quantity = document.querySelector('#quantity');/*je créé une variable pour lui donner comme référence la balise liée à l'ID quantity*/
+            let quantity = document.querySelector('#quantity');/*je créé une variable pour lui donner comme reference la balise liee à l'ID quantity*/
  
-            const EventTarget = function (product) {/*je créé une constante pour cibler les fonctions de l'élément product*/
+            const EventTarget = function (product) {/*je cree une constante pour cibler les fonctions de l'element product*/
             this.listeners = {}/*cette ecoute du produit*/
         }
             EventTarget.prototype.listeners = null/*la cible de l'element est null*/
             EventTarget.prototype.addEventlistener = function (event) {/*ecouter les fonctions de l'evenement cible avec l'evenement*/
-                if(type = product);/*si le type est égal au produit*/
-             id = value.product;/*la valeur du produit est égal à son ID*/
-             colors = value.product.colors;/*la valeur des couleurs du produit son égal à sa couleur*/
-             quantity = value.quantity;/*la valeur de quantity est égal à sa valeur*/
+                if(type = product);/*si le type est egal au produit*/
+             id = value.product;/*la valeur du produit est egal à son ID*/
+             colors = value.product.colors;/*la valeur des couleurs du produit son egal a sa couleur*/
+             quantity = value.quantity;/*la valeur de quantity est egal à sa valeur*/
     }
-  
-    let buttonToCart = document.querySelector('#addToCart');/*je créé une variable pour lui donner comme référence la balise liée à l'ID buttonToCart*/
+   
+    let buttonToCart = document.querySelector('#addToCart');/*je cree une variable pour lui donner comme reference la balise liee à l'ID buttonToCart*/
      buttonToCart.addEventListener('click', function() {/*j'ajoute un ecouteur d'evenements sur le buttonToCart, on écoute l'evenement click*/
-        buttonToCart.innerHTML = "c'est ajouté";/* le contenu de l'element est changé*/
+        buttonToCart.innerHTML = "c'est ajouté";/* le contenu de l'element est change*/
 })
-
-     
-
-        })          
-                 
-          
-
-
-
-
-
-    
-
-
-
+    document.addEventListener('DOMContentLoaded', function() {/*je cree une ecoute des fonctions chargees dans le DOM*/
+        document.getById('select[name= "id_product"]').onchange=changeEventhandler;/*lors de la gestion de l'evenement, on modifie l'id*/
+        document.querySelector('select[name= "id_colors, id_quantity]').onchange=changeEventhandler;/*lors de la gestion de l'evenement, on modifie la coleur, la quantite*/
+    }); 
+    function changeEventhandler(event) {/*cibler la fonction quand elle change*/
+        if (EventTarget.value= id) ajout('product_id');/*si l'element cible a la valeur id, ajoute l'id du produit*/
+        else return;//sinon revient
+    }
+    function changeEventhandler(event) {
+        if (EventTarget.value= id_colors) ajout('color');//si la valeur de l'element cible est id couleur, ajoute la couleur
+        else ('choice' + EventTarget + 'color');//sinon suivre le choix de la couleur
+    }
+   
+});
