09214c2f743fd48a76e47464e5ba523dc5bce9f4
veronique-large <100087668+veronique-large@users.noreply.github.com>
2022-06-08 14:50:45 +0200

    Update cart.js

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,18 +1,19 @@
 //si les donnees du produit existe dans la sessionStorage on recupere l item du produit
 let DonneesSessionStorage = JSON.parse(sessionStorage.getItem("product"));
-    if (DonneesSessionStorage){
-        DonneesSessionStorage.for.of(product => { //je cree une boucle for qui permet de repeter un certain nbre de fois le code
+    if (DonneesSessionStorage){ //je cree une condition, si il y a des donnees dans le sessionStorage
+        DonneesSessionStorage.for.of(product => { //je cree une boucle for qui permet de repeter un certain nbre de fois le code pou le produit
             document.getElementById("product").innerHTML=product.idProduct;//je recupère le produit et son id à l'interieur de Html
         })
     
 let product = {id:id, color:color, quantity:quantity}
+
 fetch("http://localhost:3000/api/products"+{itemId})
        
     
             
-           
-sessionStorage.setItem('kanap', this.product);
-
+//J enregistre les donnees(sa valeur) du produit kanap dans la sessionStorage           
+sessionStorage.setItem('kanap', this.product.innerHTML);
+//si il y a des donnees dans la sessionStorage, je les recupère
 if (sessionStorage.getItem('kanap') !==null) {
     this.product = sessionStorage.getItem('kanap') ;
 }
