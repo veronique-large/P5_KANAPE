fe22410aae77be726555ed8836d01849fad59704
veronique-large <100087668+veronique-large@users.noreply.github.com>
2022-06-08 14:28:13 +0200

    Add files via upload
    Ajout de variable seconde ligne...

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,18 +1,19 @@
-/*let limitedWidthBlock = document.querySelector(".limitedWidthBlock");
-let addButton = document.getElementById("#addToCart");
-let input = document.getElementById("order");
-
-class reservation{
-constructor() {
-    this.addButton = addButton;
-    this.input = order;
-    this.kanap = kanap;
-
-}
-}
-*/
+//si les donnees du produit existe dans la sessionStorage on recupere l item du produit
+let DonneesSessionStorage = JSON.parse(sessionStorage.getItem("product"));
+    if (DonneesSessionStorage){
+        DonneesSessionStorage.for.of(product => { //je cree une boucle for qui permet de repeter un certain nbre de fois le code
+            document.getElementById("product").innerHTML=product.idProduct;//je recupère le produit et son id à l'interieur de Html
+        })
+    
+let product = {id:id, color:color, quantity:quantity}
+fetch("http://localhost:3000/api/products"+{itemId})
+       
+    
+            
+           
 sessionStorage.setItem('kanap', this.product);
 
 if (sessionStorage.getItem('kanap') !==null) {
     this.product = sessionStorage.getItem('kanap') ;
+}
 }
