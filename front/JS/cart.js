462c7f6466f7f8cf8934c5e58690bb9e2d7cf5e8
veronique-large <100087668+veronique-large@users.noreply.github.com>
2022-06-08 18:08:20 +0200

    Add files via upload

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,20 +1,18 @@
 //si les donnees du produit existe dans la sessionStorage on recupere l item du produit
 let DonneesSessionStorage = JSON.parse(sessionStorage.getItem("product"));
-    if (DonneesSessionStorage){ //je cree une condition, si il y a des donnees dans le sessionStorage
-        DonneesSessionStorage.for.of(product => { //je cree une boucle for qui permet de repeter un certain nbre de fois le code pou le produit
-            document.getElementById("product").innerHTML=product.idProduct;//le produit recuperé dans HTML est egal au produit et son id
-        })
+    if (DonneesSessionStorage){//je cree une condition si il y a des donnees dans le sessionStorage
+        for(let kanap of product) {
+        /*    document.getElementById("product").innerHTML=product.idProduct;//le produit recuperé dans HTML est egal au produit et son ID
+        })*/
     
-let product = {id:id, color:color, quantity:quantity}
-
-fetch("http://localhost:3000/api/products"+{itemId})
-       
-    
-            
-//J enregistre les donnees(sa valeur) du produit kanap dans la sessionStorage           
+let product = {id:id, color:color, quantity:quantity} //la variable produit est composee d'un ID d'une couleur et d'une quantite
+fetch("http://localhost:3000/api/products"+id);
+                  
+           
 sessionStorage.setItem('kanap', this.product.innerHTML);
-//si il y a des donnees dans la sessionStorage, je les recupère
+
 if (sessionStorage.getItem('kanap') !==null) {
     this.product = sessionStorage.getItem('kanap') ;
 }
-}
+           }
+    }
