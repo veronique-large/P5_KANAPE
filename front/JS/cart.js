299ee09b30b4e61ccf6895093b3efcde2734ee33
veronique-large <100087668+veronique-large@users.noreply.github.com>
2022-06-20 13:43:45 +0200

    Add files via upload
    valeur de la quantité

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -98,16 +98,16 @@ if (DonneesLocalStorage){//je cree une condition si il y a des donnees dans le s
     quantity.addEventListener('change', function(event) {
         event.preventDefault();
 
-    let produit = kanap.id;
-    let produitcouleur = kanap.colors;
+    produit:kanap.id;
+    produitCouleur:kanap.colors;
 
     DonneesLocalStorage.find(p => p.id === kanap.id && p.colors === kanap.colors);
 
     if (DonneesLocalStorage){
         let rechercheProduit = DonneesLocalStorage.find(p => p.id === kanap.id && p.colors === kanap.colors); //recherche de produit , filtre du localStorage, si les produits sont strictement egaux 
         if (rechercheProduit){
-            let newQuantity = rechercheProduit.quantity +- kanap.quantity;
-            rechercheProduit.quantity = newQuantity.value;
+            let newQuantity = Number(quantity.value);
+            rechercheProduit.quantity = newQuantity;
             localStorage.setItem("kanap.quantity", JSON.stringify(DonneesLocalStorage));
         }
         else {
